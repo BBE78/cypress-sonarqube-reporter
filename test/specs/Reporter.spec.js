06546d3a63ce20b5f8ff68bf3ef7db8b0dc2face
Ricardo Luiz Gomes <ricardo.gomes@skyinformatica.com.br>
2022-05-30 14:38:15 -0300

    Renaming the spec to make its purpose clearer

--- test/specs/Reporter.spec.js
+++ test/specs/Reporter.spec.js
@@ -260,7 +260,7 @@ describe('Testing reporter', () => {
             config.config.testFiles = '**/*.spec.js';
             return cypress.run(config).then(() => {
                 verifyReport(path.resolve(testDir, 'test/cypress/integration/Sample.spec.js.xml'), config);
-                verifyReport(path.resolve(testDir, 'test/cypress/integration/Another.spec.js.xml'), config, 'test/cypress/integration/Another.spec.js');
+                verifyReport(path.resolve(testDir, 'test/cypress/integration/SampleWithNoSpecTitle.spec.js.xml'), config, 'test/cypress/integration/SampleWithNoSpecTitle.spec.js');
             }).catch(err => {
                 throw err;
             });
