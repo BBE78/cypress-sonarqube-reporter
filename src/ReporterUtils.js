ac5f003a5f89e1cd17dcd712de6644f1ddbafa3d
Raik Rohde <raik.rohde@gmail.com>
2023-09-19 10:04:30 +0200

    fix(ReporterUtils): filename extraction for component testing
    Fixes an issue with extracting the spec filename from the suite in component testing. The code now properly handles cases where the requested spec is not available and falls back to using the parent suite's file as the spec filename.

--- src/ReporterUtils.js
+++ src/ReporterUtils.js
@@ -27,6 +27,7 @@ const throwError = (message) => {
 
 /**
  * Extract the spec file path from the suite.
+ * May ignore the options.useAbsoluteSpecPath if the requested is not available.
  * Raise an error if the spec could not be extracted.
  *
  * @param {object} suite the Mocha suite
@@ -41,8 +42,10 @@ const extractSpecFromSuite = (suite, options) => {
     if (index > -1) {
         spec = JSON.parse(title.substring(index + tag.length, title.lastIndexOf(']')));
         spec = options.useAbsoluteSpecPath ? spec.absolute : spec.relative;
-    } else if (suite.invocationDetails) {
+    } else if (suite.invocationDetails && (suite.invocationDetails.absoluteFile || suite.invocationDetails.relativeFile)) {
         spec = options.useAbsoluteSpecPath ? suite.invocationDetails.absoluteFile : suite.invocationDetails.relativeFile;
+    } else if (suite.parent && suite.parent.file) {
+        spec = suite.parent.file;
     } else {
         throwError(`could not find spec filename from title: ${title} or from 'suite.invocationDetails'`);
     }
